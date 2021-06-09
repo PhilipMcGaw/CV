f363fa9c8bb9bd70debffaed9e7891e38d2161d4
Philip McGaw <Philip@McGaw.eu>
2021-06-09 12:19:01 +0100

    moved about to a sub file…

---
+++ html/sections/about.php
@@ -0,0 +1,38 @@
+<!-- About -->
+<div class="modal fade" id="About" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="Modal-label-0">About</h4>
+            </div>
+            <div class="modal-body">
+                <img src="img/me.png" style="width: 200px; height: auto;" />
+                <h2 class="rotator">Hi, I'm Philip</h2>
+
+                <h3 class="rotator">I am a<span class="rotate">&nbsp;geek_,n&nbsp;engineer_,&nbsp;maker_</span></h3>
+
+                <div class="row">
+                    <div class="col-md-9">
+                        <p>
+                          I am from London, and I studied at Imperial College. I get excited by tech, making a positive impact on Earth, and anything that pushes the boundaries of what's possible. I plan to die on Mars, <a href="https://youtu.be/LeQMWdOMa-A?t=2671" target="_blank" style="text-decoration: none; color: #555555">just hopefully not on impact!</a>
+                        </p>
+                        <p>
+                          I work at the intersection of hardware and software, of design and development. I am more than an engineer; my strong technical capability is supported by a good understanding of great design. The user comes first, everything else will follow.
+                        </p>
+                    </div>
+                    <div class="col-md-9">
+                        <p>
+                          Let me make your product real, one which will have a substantial impact on the world. Could my concept development, prototyping and realisation skills be applied to your project?
+                        </p>
+                        <p>
+                          Contact me at <a href="philip@mcgaw.eu">philip@mcgaw.eu</a> || <a href="BENCOBLEYCV.pdf" target="_blank">CV</a> || <a target="_blank" href="https://www.linkedin.com/in/bennetcobley/">LinkedIn</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
